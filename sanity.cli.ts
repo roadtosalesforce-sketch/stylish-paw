f2e561c1194aebc9c53e53b22597508748762b17
samet yazar <road.to.salesforce@gmail.com>
2026-08-13 18:05:59 +0200

    Configure embedded Sanity Studio path

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import {defineCliConfig} from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "0w20qxza",
+    dataset: "production",
+  },
+  deployment: {
+    appId: "tf396t55fyewl7283xg5k735",
+  },
+});
--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import {schemaTypes} from "./src/sanity/schemaTypes";
 export default defineConfig({
   name: "furry-fairy-pets",
   title: "Furry Fairy Pets",
+  basePath: "/studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "0w20qxza",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   plugins: [structureTool(), visionTool()],
