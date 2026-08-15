e2fb70740fde5f66fe94fabb0abec6c6af24ff64
samet yazar <road.to.salesforce@gmail.com>
2026-08-15 20:30:02 +0200

    Make site icon clearly feline

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -7,6 +7,9 @@ export const size = {
 
 export const contentType = "image/png";
 
+const cream = "#fff7ed";
+const ink = "#403230";
+
 export default function Icon() {
   return new ImageResponse(
     (
@@ -24,66 +27,154 @@ export default function Icon() {
       >
         <div
           style={{
-            background: "#fff7ed",
-            borderRadius: "50% 50% 46% 46%",
-            bottom: 10,
+            background: cream,
+            borderRadius: 4,
             display: "flex",
-            height: 27,
+            height: 20,
+            left: 12,
             position: "absolute",
-            transform: "rotate(180deg)",
-            width: 34,
+            top: 12,
+            transform: "rotate(45deg)",
+            width: 20,
           }}
         />
         <div
           style={{
-            background: "#fff7ed",
-            borderRadius: "50%",
+            background: cream,
+            borderRadius: 4,
             display: "flex",
-            height: 14,
-            left: 8,
+            height: 20,
             position: "absolute",
-            top: 23,
-            transform: "rotate(-20deg)",
-            width: 12,
+            right: 12,
+            top: 12,
+            transform: "rotate(45deg)",
+            width: 20,
           }}
         />
         <div
           style={{
-            background: "#fff7ed",
+            background: cream,
+            borderRadius: "48% 48% 44% 44%",
+            display: "flex",
+            height: 39,
+            left: 10,
+            position: "absolute",
+            top: 17,
+            width: 44,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
             borderRadius: "50%",
             display: "flex",
-            height: 16,
+            height: 6,
             left: 20,
             position: "absolute",
-            top: 12,
-            transform: "rotate(-8deg)",
-            width: 13,
+            top: 32,
+            width: 5,
           }}
         />
         <div
           style={{
-            background: "#fff7ed",
+            background: ink,
             borderRadius: "50%",
             display: "flex",
-            height: 16,
+            height: 6,
             position: "absolute",
             right: 20,
-            top: 12,
+            top: 32,
+            width: 5,
+          }}
+        />
+        <div
+          style={{
+            background: "#d9675d",
+            borderRadius: "50% 50% 60% 60%",
+            display: "flex",
+            height: 5,
+            left: 29,
+            position: "absolute",
+            top: 41,
+            width: 6,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
+            borderRadius: 2,
+            display: "flex",
+            height: 2,
+            left: 25,
+            position: "absolute",
+            top: 48,
+            transform: "rotate(-28deg)",
+            width: 8,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
+            borderRadius: 2,
+            display: "flex",
+            height: 2,
+            position: "absolute",
+            right: 25,
+            top: 48,
+            transform: "rotate(28deg)",
+            width: 8,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
+            borderRadius: 2,
+            display: "flex",
+            height: 2,
+            left: 3,
+            position: "absolute",
+            top: 40,
             transform: "rotate(8deg)",
             width: 13,
           }}
         />
         <div
           style={{
-            background: "#fff7ed",
-            borderRadius: "50%",
+            background: ink,
+            borderRadius: 2,
             display: "flex",
-            height: 14,
+            height: 2,
+            left: 3,
             position: "absolute",
-            right: 8,
-            top: 23,
-            transform: "rotate(20deg)",
-            width: 12,
+            top: 45,
+            transform: "rotate(-8deg)",
+            width: 13,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
+            borderRadius: 2,
+            display: "flex",
+            height: 2,
+            position: "absolute",
+            right: 3,
+            top: 40,
+            transform: "rotate(-8deg)",
+            width: 13,
+          }}
+        />
+        <div
+          style={{
+            background: ink,
+            borderRadius: 2,
+            display: "flex",
+            height: 2,
+            position: "absolute",
+            right: 3,
+            top: 45,
+            transform: "rotate(8deg)",
+            width: 13,
           }}
         />
       </div>
